93ca2f16ceb609cba3067ee146482f7266551d09
Dogu Yilmaz <dogukyilmaz@gmail.com>
2026-03-30 00:48:58 +0300

    feat: add dotfiles script and update package configuration

---
+++ bin/dotfiles.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+const { execFileSync } = require("child_process");
+const { join } = require("path");
+
+execFileSync("bash", [join(__dirname, "..", "setup", "collect-machine-config.sh")], { stdio: "inherit" });
